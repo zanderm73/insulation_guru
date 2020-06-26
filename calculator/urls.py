c4b4c43bb6f14b14773a3d4ad5caa5b5e3091218
zanderm73 <zanderm73@hotmail.co.uk>
2020-06-26 21:02:12 +0000

    added in the first plasterboard calculator page and created a basic navbar

--- calculator/urls.py
+++ calculator/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.calc_list, name='list')
+    path('', views.calc_list, name='list'),
+    path('plasterboard', views.board_breakdown, name='plasterboard')
 ]
